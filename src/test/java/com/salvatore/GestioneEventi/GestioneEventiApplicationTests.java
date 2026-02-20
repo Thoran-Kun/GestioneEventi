c87ef419dbf514bbf7099d8b8eb862d8d2866870
Salvatore <pepesalvatore12@gmail.com>
2026-02-20 09:36:41 +0100

    Initial commit

---
+++ src/test/java/com/salvatore/GestioneEventi/GestioneEventiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salvatore.GestioneEventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneEventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
